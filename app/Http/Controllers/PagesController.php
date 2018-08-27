2744f9fe099fd384372daa9baeb51c668b6285c3
programador 3d <programador3d@3dstudioweb.com>
2018-08-26 20:08:59 -0500

    Agregando Paginacion a la pagina principal

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,7 +11,7 @@ class PagesController extends Controller
     public function home(){
         // Asi se realiza una query scope desde el modelo
         // Sirve para lister resultados que siempre van a ser iguales
-        $posts = Post::Published()->get();
+        $posts = Post::Published()->paginate(1);
         
     	return view('welcome', compact('posts'));
     }
